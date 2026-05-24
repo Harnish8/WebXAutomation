303ed7629084bf656f809464176138d4cfd9e062
Harnish8 <concept.harnish@gmail.com>
2026-05-24 12:05:19 +0530

    update: changes

--- next.config.js
+++ next.config.js
@@ -1,28 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'lh3.googleusercontent.com',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'images.unsplash.com',
-//       },
-//     ],
-//   },
-// }
-
-// module.exports = nextConfig
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Enable Static Export
-  output: 'export',
-  trailingSlash: true,
 
-  // 2. Disable Image Optimization (Required for static hosting)
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -39,3 +18,4 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
