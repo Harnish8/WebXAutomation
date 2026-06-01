ec8068fe83c4455a2f3cff99f433abbd7cbe0734
Harnish8 <concept.harnish@gmail.com>
2026-06-01 23:55:43 +0530

    Final Live

--- app/layout.js
+++ app/layout.js
@@ -42,13 +42,16 @@ export default function RootLayout({ children }) {
           />
         </noscript>
 
-        <AuroraBackground />
-        <CustomCursor />
+        {/* <AuroraBackground /> */}
+        {/* <CustomCursor /> */}
         <Navbar />
         <main>{children}</main>
         <Footer />
         {/* reCAPTCHA v3 */}
-
+        {/* <Script
+          src="https://www.google.com/recaptcha/api.js?render=6LfGJfMsAAAAALzd9Cj2zUOlnNVLahi0q-cswIPg"
+          strategy="afterInteractive"
+        /> */}
         <CookieBanner />
       </body>
     </html>
